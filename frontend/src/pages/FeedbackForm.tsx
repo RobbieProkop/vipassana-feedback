7649733cb537488fa332fdad950d17256c6c7858
Robbie Prokop <robbieprokop@gmail.com>
2023-09-14 14:18:09 -0600

    adding more markup

--- frontend/src/pages/FeedbackForm.tsx
+++ frontend/src/pages/FeedbackForm.tsx
@@ -13,11 +13,11 @@ const FeedbackForm = () => {
             <input type="text" name="name" placeholder="Name" />
           </div>
           <div className={styles.formGroup}>
-            <label htmlFor="name">Email (optional)</label>
+            <label htmlFor="email">Email (optional)</label>
             <input type="email" name="email" placeholder="Email" />
           </div>
           <div className={styles.formGroup}>
-            <label htmlFor="name">Course Start Date *</label>
+            <label htmlFor="course-start">Course Start Date *</label>
             <input
               type="date"
               name="course-start"
@@ -27,30 +27,314 @@ const FeedbackForm = () => {
             />
           </div>
           <div className={styles.formGroup}>
-            <label htmlFor="name">Number Of Days Served *</label>
-            <textarea
-              rows={10}
-              name="days-served"
-              placeholder="Number Of Days Served"
-            />
+            <label htmlFor="days-served">Number Of Days Served *</label>
+            <input type="number" name="days-served" min={0} />
           </div>
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="name">
+          <label htmlFor="question1">
             1. What was your motivation for coming to serve?
           </label>
           <textarea rows={10} name="question1" />
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="name">2. Did you feel valued as a server?</label>
+          <label htmlFor="question2">2. Did you feel valued as a server?</label>
           <textarea rows={10} name="question2" />
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="name">
-            Are you against returning to Dhamma Karuna to serve again if your
-            time and resources permit? If yes, please tell us why.
+          <label htmlFor="question3">
+            3. Are you against returning to Dhamma Karuna to serve again if your
+            time and resources permit?
           </label>
+          <p>If yes, please tell us why.</p>
           <textarea rows={10} name="question3" />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="question4">
+            4. What prevents you from serving more frequently?
+          </label>
+
+          <textarea rows={10} name="question4" />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="question5">
+            5. Please rate your overall experience in the following: 0-Poor
+            5-Exceptional
+          </label>
+          <div className={styles.formGroupCheck}>
+            <label htmlFor="Question1">- How well were you onboarded?</label>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio1">
+                1
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio2">
+                2
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio3">
+                3
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio4">
+                4
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio5">
+                5
+              </label>
+            </div>
+          </div>
+
+          <div className={styles.formGroupCheck}>
+            <label htmlFor="Question1">
+              - Availability of required resources (tools, material etc.)
+            </label>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio1">
+                1
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio2">
+                2
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio3">
+                3
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio4">
+                4
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio5">
+                5
+              </label>
+            </div>
+          </div>
+
+          <div className={styles.formGroupCheck}>
+            <label htmlFor="Question1">- How was the food?</label>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio1">
+                1
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio2">
+                2
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio3">
+                3
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio4">
+                4
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio5">
+                5
+              </label>
+            </div>
+          </div>
+          <div className={styles.formGroupCheck}>
+            <label htmlFor="Question1">- Your overall experience</label>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio1">
+                1
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio2">
+                2
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio3">
+                3
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio4">
+                4
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio5">
+                5
+              </label>
+            </div>
+          </div>
+          <div className={styles.formGroupCheck}>
+            <label htmlFor="Question1">- How well were you onboarded?</label>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio1">
+                1
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio2">
+                2
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio3">
+                3
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio4">
+                4
+              </label>
+            </div>
+            <div className={styles.formCheck}>
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+              ></input>
+              <label className="form-check-label" htmlFor="inlineRadio5">
+                5
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.formGroup}>
+          <label htmlFor="additional">Additional Suggestions / Feedback:</label>
+          <textarea rows={10} name="Additional Info" />
         </div>
 
         <button className="btn btn-block">Submit</button>
