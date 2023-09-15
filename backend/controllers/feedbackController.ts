5da6684404b1b3c41ab3f19b93d2b3bd2279030a
Robbie Prokop <robbieprokop@gmail.com>
2023-09-15 12:18:11 -0600

    get individual feedback

--- backend/controllers/feedbackController.ts
+++ backend/controllers/feedbackController.ts
@@ -22,17 +22,33 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 const getAllFeedback = asyncHandler(async (req, res) => {
   const feedback = await sequelize.query(
     `SELECT * FROM Feedback
-  ORDER BY id DESC`,
+  ORDER BY id DESC;`,
     {
       raw: true,
       type: QueryTypes.SELECT,
     }
   );
+  res.status(200).json(feedback);
 });
 
 //Description: Get Single Feedback
 //Route: GET /api/feedback/:id
 //access: Private
-const getFeedbackById = asyncHandler(async (req, res) => {});
+const getFeedbackById = asyncHandler(async (req, res) => {
+  const feedback = await sequelize.query(
+    `SELECT * FROM Feedback
+    WHERE id = :id;`,
+    {
+      raw: true,
+      type: QueryTypes.SELECT,
+      replacements: { id: req.params.id },
+    }
+  );
+  if (!feedback) {
+    res.status(400);
+    throw new Error("Feedback not found");
+  }
+  res.status(200).json(feedback);
+});
 
 export { getAllFeedback, getFeedbackById };
