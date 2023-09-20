eb7943b06b5e167d74308d208bc4d8eaea8bf6aa
Robbie Prokop <robbieprokop@gmail.com>
2023-09-20 14:47:42 -0600

    trying to connect to db

--- backend/server.ts
+++ backend/server.ts
@@ -2,19 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import feedbackRoutes from "./routes/feedbackRoutes.js";
+import pool from "./config/db.js";
 
 const PORT = process.env.PORT || 8080;
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.log("error :>> ", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 });
 
-// app.get("/api/feedback", (req, res) => {
-//   res.json({ message: "Hello from server!" });
-// });
+app.get("/api/feedback", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
 
 app.get("/api/feedback/:id", (req, res) => {
   const feedback = "Some feedback here";
