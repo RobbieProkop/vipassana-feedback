26cd9ce326cbbdaa7c83a34f6c38529bc54d7cfe
Robbie Prokop <robbieprokop@gmail.com>
2023-09-20 22:08:57 -0600

    create adminMiddleware

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -17,10 +17,21 @@ const protect = asyncHandler(async (req, res, next) => {
     req.user = await User.findByPk(decoded.id, {
       attributes: { exclude: ["password"] },
     });
+    next();
   } catch (error) {
+    console.log("error :>> ", error);
     res.status(401);
     throw new Error("Not authorized, token failed");
   }
 });
 
-export default { protect };
+const adminMiddleware = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
+
+export { protect, adminMiddleware };
