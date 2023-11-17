da66de178495ea281cef9849b0ec2d158682dce0
Robbie Prokop <robbieprokop@gmail.com>
2023-11-17 15:02:34 -0700

    style feedbackDashboard

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -4,15 +4,16 @@ import styles from "./card.module.scss";
 
 // FeedbackCard component
 const Card: React.FC<{ feedback: Feedback }> = ({ feedback }) => {
-  const { course_start_date, submitted_at, name } = feedback;
+  const { course_start_date, submitted_at, name, id } = feedback;
 
   const submitted = submitted_at.split("T")[0];
 
   return (
     <div className={styles.feedbackCard}>
-      <Link to={`/feedback/${feedback.id}`} className={styles.link}>
+      <Link to={`/admin/feedback/${feedback.id}`} className={styles.link}>
+        <p>ID: {id}</p>
         <h2>Name: {name || "Anonymous"}</h2>
-        <h3>Course Start Date: {course_start_date}</h3>
+        <h3>Course Start: {course_start_date}</h3>
         <h3>Submitted On: {submitted}</h3>
       </Link>
     </div>
