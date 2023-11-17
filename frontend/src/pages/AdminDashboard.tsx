808f202bb638a59cee7e19d7573a7db0277f7dcc
Robbie Prokop <robbieprokop@gmail.com>
2023-11-17 11:42:57 -0700

    handle date state change

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,47 +1,64 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ErrorPage from "./ErrorPage";
+
 import axios from "axios";
 import styles from "../styles/feedbackDashboard.module.scss";
+import { BASE_URL } from "../constants";
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
 
+  interface datesState {
+    startDate: string;
+    endDate: string;
+  }
+
   const userInfo = localStorage.getItem("userInfo")
     ? JSON.parse(localStorage.getItem("userInfo")!)
     : null;
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, []);
 
   let feedback;
 
   const date = new Date();
   const today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
 
-  useEffect(() => {
-    if (!userInfo) {
-      navigate("/");
-    }
+  const [formData, setFormData] = useState<datesState>({
+    startDate: "",
+    endDate: today,
+  });
+  const { startDate, endDate } = formData;
 
-    axios.get("/api/feedback").then((res) => {
+  const onClick = async () => {
+    await axios.get(`${BASE_URL}/api/feedback`).then((res) => {
       feedback = res.data;
       console.log("feedback", feedback);
     });
-  }, []);
-  if (!userInfo) {
-    return <ErrorPage />;
-  }
+  };
 
-  const onClick = () => {};
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
   return (
     <div className="container">
       <h2>Retrieve Feedback Within Specified Period</h2>
       <div className={styles.dates}>
         <div className="formGroup">
           <label>Start Date</label>
-          <input type="date" />
+          <input type="date" value={startDate} onChange={onChange} />
         </div>
         <div className="formGroup">
           <label>End Date</label>
-          <input type="date" value={today} />
+          <input type="date" value={endDate} onChange={onChange} />
         </div>
       </div>
       <div className={styles.button}>
